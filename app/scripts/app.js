9eacce5781821e5a5c65c96d9cd45e014c373964
Cephas Lin <cephalin@microsoft.com>
2018-02-24 01:49:36 +0100

    .

---
+++ app/scripts/app.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+angular.module('todoApp', ['ngRoute'])
+.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+    $routeProvider.when("/Home", {
+        controller: "todoListCtrl",
+        templateUrl: "/App/Views/TodoList.html",
+    }).otherwise({ redirectTo: "/Home" });
+
+    }]);
---
+++ app/scripts/todoListCtrl.js
@@ -0,0 +1,72 @@
+﻿'use strict';
+angular.module('todoApp')
+.controller('todoListCtrl', ['$scope', '$location', 'todoListSvc', function ($scope, $location, todoListSvc) {
+    $scope.error = "";
+    $scope.loadingMessage = "Loading...";
+    $scope.todoList = null;
+    $scope.editingInProgress = false;
+    $scope.newToDoName = "";
+
+
+    $scope.editInProgressTodo = {
+        Name: "",
+        IsComplete: false,
+        Id: 0
+    };
+
+    
+
+    $scope.editSwitch = function (todo) {
+        todo.edit = !todo.edit;
+        if (todo.edit) {
+            $scope.editInProgressTodo.Name = todo.Name;
+            $scope.editInProgressTodo.ID = todo.Id;
+            $scope.editingInProgress = true;
+        } else {
+            $scope.editingInProgress = false;
+        }
+    };
+
+    $scope.populate = function () {
+        todoListSvc.getItems().success(function (results) {
+            $scope.todoList = results;
+            $scope.loadingMessage = "";
+        }).error(function (err) {
+            $scope.error = err;
+            $scope.loadingMessage = "";
+        })
+    };
+    $scope.delete = function (id) {
+        todoListSvc.deleteItem(id).success(function (results) {
+            $scope.loadingMessage = "";
+            $scope.populate();
+        }).error(function (err) {
+            $scope.error = err;
+            $scope.loadingMessage = "";
+        })
+    };
+    $scope.update = function (todo) {
+        todoListSvc.putItem($scope.editInProgressTodo).success(function (results) {
+            $scope.loadingMsg = "";
+            $scope.populate();
+            $scope.editSwitch(todo);
+        }).error(function (err) {
+            $scope.error = err;
+            $scope.loadingMessage = "";
+        })
+    };
+    $scope.add = function () {
+        todoListSvc.postItem({
+            'Name': $scope.newToDoName,
+            'IsComplete': false
+        }).success(function (results) {
+            $scope.loadingMsg = "";
+            $scope.newToDoName = "";
+            $scope.populate();
+        }).error(function (err) {
+            $scope.error = err;
+            $scope.loadingMsg = "";
+        })
+    };
+
+}]);
